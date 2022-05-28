cb22a15d58896fd7787e40933ff7d312190c84aa
Serhii Kyrylenko <32450732+Devepre@users.noreply.github.com>
2022-05-28 13:14:50 +0300

    framework init

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "MyFramework",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: "MyFramework", targets: ["MyFramework"])
+    ],
+    targets: [
+        .target(
+            name: "Kingfisher",
+            path: "./MyFramework.xcframework"
+        )
+    ]
+)
